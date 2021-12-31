b4a832b684204666cc69eeb5084926f23bf876a8
Shourya Naik <sjnaik02@gmail.com>
2021-12-31 19:04:22 +0530

    added break/session length and related buttons

--- src/index.js
+++ src/index.js
@@ -1,20 +1,53 @@
 import React from "react";
 import ReactDOM from 'react-dom'
+import './index.css'
 
-class App extends React.Component{
-    constructor(props){
+class App extends React.Component {
+    constructor(props) {
         super(props);
         this.state = {
-            sessionLength : 1500,
-            breakLength : 300,
-            isBreak : false,
-            isPaused : true, 
+            sessionLength: 25,
+            breakLength: 5,
+            isBreak: false,
+            isPaused: true,
+            currentTimerLength : '',
         }
     }
 
-    render(){
-        return(
-            <h1> Hello World </h1>
+    increment(type){
+        if(type === 'session' && this.state.sessionLength<60){
+            this.setState((state) => ({
+                sessionLength : state.sessionLength + 1
+            }));
+        } else if(type === 'break' && this.state.breakLength<60){
+            this.setState((state) => ({
+                breakLength : state.breakLength + 1
+            }));
+        }
+
+    }
+
+    decrement(type){
+        if(type === 'session' && this.state.sessionLength>1){
+            this.setState((state) => ({
+                sessionLength : state.sessionLength - 1
+            }));
+        } else if(type === 'break' && this.state.breakLength>1){
+            this.setState((state) => ({
+                breakLength : state.breakLength - 1
+            }));
+        }
+    }
+
+    render() {
+        return (
+            <div id='wrapper'>
+                <h1> 25/5 Timer </h1>
+                <h2> Session Length: {this.state.sessionLength} </h2>
+                <button onClick={() => this.increment('session')}> inc </button> <button onClick={() => this.decrement('session')}> dec </button>
+                <h2> Break Length: {this.state.breakLength} </h2>
+                <button onClick={() => this.increment('break')}> inc </button> <button onClick={() => this.decrement('break')}> dec </button>
+            </div>
         );
     }
 }
