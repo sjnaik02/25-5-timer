45a5d2c95bf44c8d791c86bf1f3f9d6e01ab0a36
Shourya Naik <sjnaik02@gmail.com>
2021-12-31 12:04:22 +0530

    set up boilerplate and state

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            sessionLength : 1500,
+            breakLength : 300,
+            isBreak : false,
+            isPaused : true, 
+        }
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    render(){
+        return(
+            <h1> Hello World </h1>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
