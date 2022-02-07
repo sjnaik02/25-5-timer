b467f43f8b9c58551e276d1103a0ebb81971eac9
Shourya Naik <sjnaik02@gmail.com>
2022-02-07 19:28:33 +0530

    added buttons and labels with ids

--- src/index.js
+++ src/index.js
@@ -6,17 +6,23 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            'session-length' : 25,
+            'break-length' : 5
         }
-        
     }
-
-    
 
     render() {
         return (
             <div id='wrapper'>
                 <h1> 25/5 Timer </h1>
+                <h2 id="session-label"> Session Length : {this.state["session-length"]}</h2>
+                <button id="session-increment"> Increment </button> 
+                <button id="session-decrement"> Decrement </button>
+
+                <h2 id="break-label"> Break Length : {this.state["break-length"]}</h2>
+                <button id="break-increment"> Increment </button> 
+                <button id="break-decrement"> Decrement </button>
+
                 
             </div>
         );
