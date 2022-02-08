74c442d49e8330aa3af40b40858d8f0816afcffb
Shourya Naik <sjnaik02@gmail.com>
2022-02-08 09:06:53 +0530

    added accurate interval function

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-const Timer = (props) =>{
-  return(
-    <h1>Hello</h1>
+(function() {
+  window.accurateInterval = function(time, fn) {
+    var cancel, nextAt, timeout, wrapper, _ref;
+    nextAt = new Date().getTime() + time;
+    timeout = null;
+    if (typeof time === 'function') _ref = [time, fn], fn = _ref[0], time = _ref[1];
+    wrapper = function() {
+      nextAt += time;
+      timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+      return fn();
+    };
+    cancel = function() {
+      return clearTimeout(timeout);
+    };
+    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
+    return {
+      cancel: cancel
+    };
+  };
+}).call(this);
+
+function Timer({ sessionLen, breakLen }) {
+  const [length, setLength] = useState(sessionLen);
+  const [isPaused, setPaused] = useState(true);
+  let intervalRef = useRef();
+
+
+
+  return (
+    <>
+      <h3>{length}</h3>
+      <button onClick={() => playPause()}> Play / Pause </button>
+    </>
   );
 }
 
