4f40c4719d9ae53f66cf71eb7d37a7ae7d471ac7
Shourya Naik <sjnaik02@gmail.com>
2022-02-08 09:10:04 +0530

    moved accurateInterval to seperate file

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,25 +1,5 @@
 import React, { useState, useRef } from "react";
-
-(function() {
-  window.accurateInterval = function(time, fn) {
-    var cancel, nextAt, timeout, wrapper, _ref;
-    nextAt = new Date().getTime() + time;
-    timeout = null;
-    if (typeof time === 'function') _ref = [time, fn], fn = _ref[0], time = _ref[1];
-    wrapper = function() {
-      nextAt += time;
-      timeout = setTimeout(wrapper, nextAt - new Date().getTime());
-      return fn();
-    };
-    cancel = function() {
-      return clearTimeout(timeout);
-    };
-    timeout = setTimeout(wrapper, nextAt - new Date().getTime());
-    return {
-      cancel: cancel
-    };
-  };
-}).call(this);
+import accurateInterval from "../utils/accurateInterval";
 
 function Timer({ sessionLen, breakLen }) {
   const [length, setLength] = useState(sessionLen);
