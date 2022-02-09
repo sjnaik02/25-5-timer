45e2a392277b64c909281aed9a283fb363625b6e
Shourya Naik <sjnaik02@gmail.com>
2022-02-09 18:26:47 +0530

    added better styling for display

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,10 +3,13 @@ import React from "react";
 const Display = ({ label, length, incrementor, decrementor }) => {
   return (
     <>
-      <h2 className="text-center" id={label.concat("-label")}> {label} length: </h2><h2 className="text-center" id={label.concat("-length")}>{length}</h2>
-      <div className="w-full flex justify-around">
-        <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-increment")} onClick={() => incrementor(label)}> + </button>
-        <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-decrement")} onClick={() => decrementor(label)}> - </button>
+      <div className="flex flex-col m-4">
+        <h2 className="text-center" id={label.concat("-label")}> {label} length: </h2>
+        <h2 className="text-center text-lg" id={label.concat("-length")}>{length}</h2>
+        <div className="w-full flex justify-around">
+          <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-increment")} onClick={() => incrementor(label)}> + </button>
+          <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-decrement")} onClick={() => decrementor(label)}> - </button>
+        </div>
       </div>
     </>
   )
