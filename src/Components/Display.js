97cb913772b5770f683de48e0e8d4a7539dc5f80
Shourya Naik <sjnaik02@gmail.com>
2022-02-09 11:58:32 +0530

    added styles to buttons, display element, etc

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,9 +3,11 @@ import React from "react";
 const Display = ({ label, length, incrementor, decrementor }) => {
   return (
     <>
-      <h2 id={label.concat("-label")}> {label} length: </h2><h2 id={label.concat("-length")}>{length}</h2>
-      <button id={label.concat("-increment")} onClick={() => incrementor(label)}> Increment </button>
-      <button id={label.concat("-decrement")} onClick={() => decrementor(label)}> Decrement </button>
+      <h2 className="text-center" id={label.concat("-label")}> {label} length: </h2><h2 className="text-center" id={label.concat("-length")}>{length}</h2>
+      <div className="w-full flex justify-around">
+        <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-increment")} onClick={() => incrementor(label)}> + </button>
+        <button className="border-2 rounded-full w-12 h-12 m-2 border-emerald-500 hover:bg-emerald-500 active:bg-emerald-500 transition-colors" id={label.concat("-decrement")} onClick={() => decrementor(label)}> - </button>
+      </div>
     </>
   )
 }
