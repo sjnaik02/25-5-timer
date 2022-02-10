8502da811325f3f70b9ec4a04d97a8312ee91da3
Shourya Naik <sjnaik02@gmail.com>
2022-02-10 12:15:01 +0530

    fixed links

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -4,8 +4,8 @@ import '../index.css';
 const Info = () => {
   return(
     <div className="my-4">
-    <p className="text-emerald-500 mx-auto"> Code for this can be found <a className="text-emerald-300" href="https://github.com/sjnaik02/25-5-timer">here</a> </p>
-    <p className="text-emerald-500 mx-auto"> Created by <a className="text-emerald-300" href="www.shouryanaik.com" >Shourya Naik</a> </p>
+    <p className="text-emerald-500 mx-auto"> Code for this can be found <a className="text-emerald-300" href="https://github.com/sjnaik02/25-5-timer" target="_blank" rel="noreferrer">here</a> </p>
+    <p className="text-emerald-500 mx-auto"> Created by <a className="text-emerald-300" href="https://shouryanaik.com" target="_blank" rel="noreferrer" >Shourya Naik</a> </p>
     </div>
   );
 }
